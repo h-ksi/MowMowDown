b6995d01c3df9301ac517caeff5623ce8e10215e
h-ksi <mathko314@gmail.com>
2019-04-14 11:31:30 +0900

    Scripts relevant to HealthSystem(HP, damage, recovery, etc.)

---
+++ Assets/Scripts/Utility/HealthSystem/Health.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UniRx;
+
+public class Health : IHealth
+{
+	public float MaxHP { get; private set; } = 100;
+
+	public ReactiveProperty<float> HP { get; private set; } = new ReactiveProperty<float>();
+
+	public float Rate
+	{
+		get
+		{
+			if (MaxHP <= 0)
+				return 0f;
+			return (float)HP.Value / (float)MaxHP;
+		}
+	}
+
+	public Health(float maxHP = 100)
+	{
+		Init(maxHP);
+	}
+
+	/// <summary>
+	/// Initialization
+	/// </summary>
+	public void Init(float maxHP = 100)
+	{
+		this.MaxHP = maxHP;
+		this.HP = new ReactiveProperty<float>(MaxHP);
+	}
+
+	/// <summary>
+	/// Subtract damage point from HP
+	/// </summary>
+	public void TakeDamage(float damage)
+	{
+		HP.Value -= damage;
+	}
+
+	/// <summary>
+	/// Add heal point to HP
+	/// </summary>
+	public void Recovery(float heal)
+	{
+		HP.Value += heal;
+	}
+}
---
+++ Assets/Scripts/Utility/HealthSystem/HealthManager.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UniRx;
+using UniRx.Triggers;
+
+public class HealthManager : MonoBehaviour
+{
+	[SerializeField] HPGauge _hpGauge;
+
+	[SerializeField] float _maxHP = 100f;
+
+	[SerializeField, Header("Way of Release() after death")]
+	AfterDeath _afetrDeath = default;
+	[SerializeField] GameObject _body;
+
+	[SerializeField, Header("Time from death until Release() is called")]
+	float _deathWaitTime = 0.0f;
+
+	[SerializeField, Header("UnityEvent called when dying")]
+	UnityEvent Die = new UnityEvent();
+
+
+	enum AfterDeath
+	{
+		Destory,
+		Inactivate
+	}
+
+
+	Health _health = new Health();
+
+
+	void OnEnable()
+	{
+		_health = new Health(_maxHP);
+
+		if (_hpGauge != null)
+			_hpGauge.SetMaxHP(_maxHP);
+	}
+
+
+	void Start()
+	{
+		// Die event
+		_health.HP
+			.First(hp => hp <= 0f)
+			.Subscribe(hp =>
+				Die.Invoke()
+			);
+
+		// HP gauge
+		_health.HP
+			.Where(_ => _hpGauge != null)
+			.Subscribe(hp =>
+				_hpGauge.ReflectOnSlider(hp)
+			);
+
+		// After death process
+		Die.AsObservable()
+			.Delay(TimeSpan.FromSeconds(_deathWaitTime))
+			.Subscribe(_ =>
+				Release()
+			);
+	}
+
+
+	void Release()
+	{
+		if (_body == null)
+			_body = gameObject;
+
+		if (_afetrDeath == AfterDeath.Destory)
+		{
+			Destroy(_body);
+		}
+		else
+		{
+			_body.SetActive(false);
+		}
+	}
+
+	/// <summary>
+	/// Initialization
+	/// </summary>
+	public void Init(float maxHP = 100f)
+	{
+		_health.Init(maxHP);
+	}
+
+	/// <summary>
+	/// Subtract damage point from HP
+	/// </summary>
+	public void TakeDamage(float damage)
+	{
+		_health.TakeDamage(damage);
+	}
+
+	/// <summary>
+	/// Add heal point to HP
+	/// </summary>
+	public void Recovery(float heal)
+	{
+		_health.Recovery(heal);
+	}
+}

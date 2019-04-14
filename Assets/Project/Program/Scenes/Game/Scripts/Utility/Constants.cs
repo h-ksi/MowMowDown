842081024ba0cf0c1646309f2822ad9189547bee
h-ksi <mathko314@gmail.com>
2019-04-15 01:56:41 +0900

    Utility scripts (Camera, HealthSystem)

---
+++ Assets/Project/Program/Scenes/Game/Scripts/Utility/Constants.cs
@@ -0,0 +1,27 @@
+﻿public static class Constants
+{
+	// Physical Attack Points
+	public const int JAB_AP = 1;
+	public const int HIKICK_AP = 5;
+
+	// Attack Points of Character's Special Move
+	public const int UNITY_SLIDE = 20;
+	public const int UNITY_BEAM = 30;
+
+	// Weapon Attack Points
+	public const int TACKLE_AP = 10;
+	public const int ROCK_THROW_AP = 12;
+	public const int BULLET_SARDINE = 1;
+	// public const int 
+	// public const int 
+	// public const int 
+	// public const int 
+
+/// <summary>
+/// Orc
+/// </summary>
+	public const float ORC_WALK_SPEED = 2.1f;
+	public const float ORC_RUN_SPEED = 4.1f;
+	public const float ORC_DEFAULT_ATK = 10f;
+	// public const float ORC_
+}
